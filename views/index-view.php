769cb5603c8e3e693339630092f16a8be0160757
MarianMdna <mecuevas81@gmail.com>
2021-02-03 10:57:29 -0300

    instale en localhost

--- views/index-view.php
+++ views/index-view.php
@@ -1,9 +1,23 @@
 <?php
 require 'views/header.php';
 ?>
+<div class="login-panel">
+<form class="formulario" name="login" action="" method="POST">
+			<div class="form-group">
+				<i class="icono izquierda fa fa-user"></i><input class="usuario" type="text" name="usuario" placeholder="Tu email">
+			</div>
 
-<h1>Hola Mundo</h1>
-<img src="<?php echo RUTA; ?>/images/teamPRO-logo.png" alt="">
+			<div class="form-group">
+				<i class="icono izquierda fa fa-lock"></i><input class="password_btn" type="password" name="password" placeholder="Contraseña">
+				<i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+			</div>
+            <a href="#">¿Olvidaste la contraseña?</a>
+		</form>
+
+
+</div>
+
+
 
 <?php
 require 'footer.php';
