796e739a5807a2ddc17df134472e9a292e1a1673
gabrielgonzalezcastellvi <50787949+gabrielgonzalezcastellvi@users.noreply.github.com>
2021-02-02 21:44:51 -0300

    https://github.com/gabrielgonzalezcastellvi/team-pro.git

--- send.php
+++ send.php
@@ -7,15 +7,40 @@ $apellido=$_POST['LastName'];
 $numeroSocio=$_POST['UserNumber'];
 $email=$_POST['email'];
 $password=$_POST['password'];
-$password = hash('sha512' , $password);
+$passwordSecure = hash('sha512' , $password);
 
 try {
-    $conexion = new PDO('mysql:host=localhost;dbname=redglobe', 'root', '');
+    $conexion = new PDO('mysql:host=localhost;dbname=registros', 'root', '');
 } catch (PDOException $e) {
     echo "Error:" . $e->getMessage();
 }
 }
 
+$asunto="Gracias Por Confiar en Team Pro";
+$mensaje2 ="
+
+
+Team Pro Pruebas de envio
+Para: $nombre  $apellido
+
+$mensaje
+
+ Señor/ra : $nombre le agradecemos confiar en nosotros su cuenta ya se creo correctamente
+ Su numero de socio es: $numeroSocio
+ recuerde que sus datos son los siguientes: $nombre , $apellido , y su contraseña es: $password que en nuestra base de datos se registra encriptada por su seguridad
+ Clave encriptada es: $passwordSecure
+ 
+";
+$header = 'From:gonzalezcastellvigabriel@gmail.com'. "\r\n" .
+'X-Mailer: PHP/' ;
+
+//mail($para,$asunto,$mensaje,$header);
+$resultado=mail($email,$asunto,$mensaje2,$header);
+    if ($resultado) {
+        echo "Correo enviado";
+    } else {
+        echo "Correo NO enviado";
+    }
 
 
 ?>
