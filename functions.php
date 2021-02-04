3283833042a7aa19e0d153d6843522a9076c0a45
MarianMdna <mecuevas81@gmail.com>
2021-02-04 11:57:19 -0300

    functions y send

--- functions.php
+++ functions.php
@@ -2,4 +2,18 @@
 
 #archivo para escribir todas las funciones y poder llamarlas
 
+
+#limpiamos datos de los formularios
+function limpiarDatos($datos){
+	// Eliminamos los espacios en blanco al inicio y final de la cadena
+	$datos = trim($datos);
+
+	// Quitamos las barras / escapandolas con comillas
+	$datos = stripslashes($datos);
+
+	// Convertimos caracteres especiales en entidades HTML (&, "", '', <, >)
+	$datos = htmlspecialchars($datos);
+	return $datos;
+}
+
 ?>
--- send.php
+++ send.php
@@ -1,12 +1,13 @@
 <?php
 require 'conection.php';
+require 'functions.php';
 
 //Se recuperan las variables del formularios de inscripcion;
-    $nombre=$_POST['Name'];
-$apellido=$_POST['LastName'];
-$numeroSocio=$_POST['UserNumber'];
-$email=$_POST['email'];
-$mobile=$_POST['mobile'];
+    $nombre=strtoupper(limpiarDatos($_POST['Name']));
+$apellido=strtoupper(limpiarDatos($_POST['LastName']));
+$numeroSocio=limpiarDatos($_POST['UserNumber']);
+$email=limpiarDatos($_POST['email']);
+$mobile=limpiarDatos($_POST['mobile']);
 $password=$_POST['password'];
 $passwordSecure = hash('sha512' , $password);
 
@@ -20,32 +21,34 @@ $sql="INSERT INTO socios VALUES (NULL,'$nombre','$apellido','$numeroSocio','$ema
 $ejecutar = mysqli_query($conectar,$sql);
 //verificamos la ejecucion;
 if(!$ejecutar){
-    echo "<script>alert('Hubo un error, por favor inténtalo nuevamente. Si el error persiste por favor contáctate con nosotros')</script>"; //luego vemos bien a donde tendria que ir esto es para probar que se envian los datos correctamente;
-    header('Location: index.php');
+    echo "<script>alert('Hubo un error, por favor inténtalo nuevamente. Si el error persiste por favor contáctate con nosotros')</script>"; 
+    
 }else{
     echo "<script>alert('Tus datos se enviaron correctamente. Por favor, chequea tu mail.')</script>";
-    header('Location: index.php');
+    
 }
 //aca abajo se envia el correo al usuario para que tambien tenga en su correo los datos de su cuenta;
 $to = $email;
 $asunto='Registro de socio en la app de reservas';
 $mensaje ="
 Hola $nombre!
-Tus datos fueron cargados correctamente, si coinciden con nuestros registros tendrás acceso a la aplicación próximamente. No te preocupes, te avisaremos. 
-Recuerda tus datos de registro:
-$nombre $apellido
-Nº socio: $numeroSocio
-Contraseña: $password
+Tus datos fueron cargados correctamente, si coinciden con nuestros registros de socios tendrás acceso a la aplicación próximamente. No te preocupes, te avisaremos. 
+
+Recuerda tus datos:
+- $nombre $apellido
+- Nº socio: $numeroSocio
+- Contraseña: $password
 
 ";
-$header = 'From: Secretaría Club TeamPRO'. "\r\n" .
+$header = 'From: Secretaría Club X'. "\r\n" .
 'X-Mailer: PHP';
 
 //mail($para,$asunto,$mensaje,$header);
-$correo=mail($email,$asunto,$mensaje,$header);
-    if ($correo) {
-        echo "<script>alert('mensaje enviado')</script>";
-    } else {
-        echo "<script>alert('Correo NO enviado Intentelo nuevamente')</script>";
-    }
+$correo=mail($to,$asunto,$mensaje,$header);
+header('Location: index.php');    
+//if ($correo) {
+//        header('Location: index.php');
+//    } else {
+//        header('Location: index.php');
+//    }
 ?>
